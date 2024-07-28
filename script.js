f384c9cfa4c620077148891afc0c6c9978a2747c
HeavenxMC <113349236+HeavenxMC@users.noreply.github.com>
2024-07-28 15:17:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("JUL 28, 2024 08:00:00").getTime();
+    const countDate = new Date("AUG 15, 2024 08:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
